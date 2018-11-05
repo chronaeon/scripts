d5dd7387f9ba9893e1fb66a09a0e2561bb7489a0
Micah Dameron <micahdameron@gmail.com>
2018-11-05 10:54:18 -0500

    modified:   loadStdLibs.r

--- R/loadStdLibs.r
+++ R/loadStdLibs.r
@@ -2,6 +2,9 @@ library(readtext)
 install.packages("readtext")
 # for reading text files
 
+install.packages("quanteda")
+#extended functionality for readtext
+
 library(stringr)
 #for string manipulation
 
