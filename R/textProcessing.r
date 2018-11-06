93d73ef7fbc3bb0e74ddc7a1dd7438ddfc8a53f4
Micah Dameron <micahdameron@gmail.com>
2018-11-06 18:43:13 -0500

    new file:   R/cppLibs/Generate_Document_Word_Matrix.cpp 	modified:   R/textProcessing.r

--- R/textProcessing.r
+++ R/textProcessing.r
@@ -1,53 +1,24 @@
-#function(clean_string)
+#for matrixizing Solidity files
 
-Clean_String <- function(string){
-    # Lowercase
-    temp <- tolower(string)
-    # Remove everything that is not a number or letter (may want to keep more 
-    # stuff in your actual analyses). 
-    temp <- stringr::str_replace_all(temp,"[^a-zA-Z\\s]", " ")
-    # Shrink down to just one white space
-    temp <- stringr::str_replace_all(temp,"[\\s]+", " ")
-    # Split it
-    temp <- stringr::str_split(temp, " ")[[1]]
-    # Get rid of trailing "" if necessary
-    indexes <- which(temp == "")
-    if(length(indexes) > 0){
-      temp <- temp[-indexes]
-    } 
-    return(temp)
-}
+library(stringr)
 
-#function(clean_text)
-# function to clean text
-Clean_Text_Block <- function(text){
-    # Get rid of blank lines
-    indexes <- which(text == "")
-    if (length(indexes) > 0) {
-        text <- text[-indexes]
-    }
-	# See if we are left with any valid text:
-    if (length(text) == 0) {
-        cat("There was no text in this document! \n")
-        to_return <- list(num_tokens = 0, 
-		                     unique_tokens = 0, 
-							 text = "")
-    } else {
-        # If there is valid text, process it.
-        # Loop through the lines in the text and combine them:
-        clean_text <- NULL
-        for (i in 1:length(text)) {
-            # add them to a vector 
-            clean_text <- c(clean_text, Clean_String(text[i]))
-        }
-        # Calculate the number of tokens and unique tokens and return them in a 
-        # named list object.
-        num_tok <- length(clean_text)
-        num_uniq <- length(unique(clean_text))
-        to_return <- list(num_tokens = num_tok, 
-		                     unique_tokens = num_uniq, 
-							 text = clean_text)
-    }
-	
-    return(to_return)
-}
+solList <- list.files(path = "~/my_audits/set-protocol-contracts/contracts", pattern = "\\.sol$", recursive = TRUE, full.names = TRUE)
+
+flatCode <- lapply(solList, readLines)
+
+flatCodeNumbers <- str_extract_all(flatCode,"[0-9]+")
+
+vector <- unlist(flatCodeNumbers)
+
+numbers <- as.numeric(vector)
+
+sortedNumbers <-  sort.int(numbers)
+
+library(plyr)
+
+countedNumbers <- count(numbers)
+
+library(ggplot2)
+theme_set(theme_pubr())
+library(dplyr)
+
