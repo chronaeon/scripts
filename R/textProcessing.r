afe726ed0d9b31ef60529005f002cce71ba4bc53
Micah Dameron <micahdameron@gmail.com>
2018-11-07 16:17:00 -0500

    modified:   R/textProcessing.r

--- R/textProcessing.r
+++ R/textProcessing.r
@@ -4,9 +4,12 @@ library(stringr)
 
 solList <- list.files(path = "~/my_audits/set-protocol-contracts/contracts", pattern = "\\.sol$", recursive = TRUE, full.names = TRUE)
 
+#flatten the Solidity files representing each as a single character vector
 flatCode <- lapply(solList, readLines)
 
+#extract all numbers in the code
 flatCodeNumbers <- str_extract_all(flatCode,"[0-9]+")
+flatCodeNumeric<-as.numeric(flatCodeNumbers)
 
 vector <- unlist(flatCodeNumbers)
 
@@ -18,7 +21,10 @@ library(plyr)
 
 countedNumbers <- count(numbers)
 
-library(ggplot2)
-theme_set(theme_pubr())
 library(dplyr)
 
+str_count(flatCode, "withdraw")
+#find all functions with "withdraw" in them
+
+str_count(flatCode, "deposit")
+#find all functions with "deposit" in them
