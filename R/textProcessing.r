077311899c5e25e70b8666c0642b037f311c8e93
Micah Dameron <micahdameron@gmail.com>
2019-03-11 17:23:41 -0400

    modified:   R/textProcessing.r 	new file:   Rplot.pdf

--- R/textProcessing.r
+++ R/textProcessing.r
@@ -11,14 +11,15 @@ rm(new.packages)
 
 solList <- list.files(path = "~/set-protocol-contracts/contracts", pattern = "\\.sol$", recursive = TRUE, full.names = TRUE)
 
+
 solListFiltered <- gsub("^.*DappHub*$|^.*0x.*$|^.*test.*$|^.*kyber.*$|^.*mocks.*$|^.*node_modules.*$|^.*Migrations.*$|^.*coverageEnv.*$|^.*openzeppelin-solidity.*$|^.*cryptofin-solidity.*$|^.*0x.*$|^.*IERC20.*$|^.Bytes32.sol.*$|^.*Authorizable.sol.*$|^.*AddressArrayUtils.sol.*$|^.*CommonMath.sol.*$", "", solList, ignore.case = TRUE)
 solListClean <- solListFiltered[solListFiltered !=""]
+fileNames <- str_extract(solListClean, "([0-9a-zA-Z\\._-]+.(sol))")
 flatSol <- lapply(solListClean, readLines)
 cleanCode <- gsub("(?s)\\/\\*.*?\\*\\/|\\/\\/.*", "", flatSol, ignore.case = TRUE, perl = TRUE)
 cleanerCode <- cleanCode[cleanCode !=""]
 #flatten the Solidity files representing each as a single character vector
 #solListFiltered <- lapply(solListClean, readLines)
-
 
 
 #extract all numbers in the code
@@ -52,11 +53,45 @@ flatNgrams$id <- rownames(flatNgrams)
 meltedNgrams<-melt(flatNgrams)
 meltedDf<-as.data.frame(meltedNgrams)
 rm(meltedNgrams)
-str_count(flatSol, "withdraw")
-#find all functions with "withdraw" in them
 
-str_count(flatSol, "deposit")
-#find all functions with "deposit" in them
+#count word occurrences
 
-str_count(flatSol, "transfer")
+withdraws <- str_count(flatSol, "withdraw")
+
+#sort withdraws
+withdrawsNz <- withdraws [nzchar(gsub("[0]", "", withdraws))]
+withdrawsValDecreasing <- order(withdrawsNz, decreasing = TRUE)
+
+withdrawsGz <- which(withdraws > 0, useNames = TRUE)
+withdrawsKey <- fileNames[withdrawsGz]
+withdrawsKeyDecreasing <- order(withdrawsKey, decreasing = TRUE)
+
+barplot(as.matrix(withdrawsNz), main = "Occurrences", ylab="Total", beside = TRUE, col=rainbow(12), legend = (withdrawsKey))
+
+#sort deposits
+deposits <- str_count(flatSol, "deposit")
+
+depositsNz <- deposits [nzchar(gsub("[0]", "", deposits))]
+depositsValDecreasing <- order(depositsNz, decreasing = TRUE)
+
+depositsGz <- which(deposits > 0, useNames = TRUE)
+depositsKey <- fileNames[depositsGz]
+depositsKeyDecreasing <- order(depositsKey, decreasing = TRUE)
+
+barplot(as.matrix(depositsNz), main = "Occurrences", ylab="Total", beside = TRUE, col=rainbow(12), legend = (depositsKey))
+
+#sort transfers
+transfers <- str_count(flatSol, "transfer")
+
+transfersNz <- transfers [nzchar(gsub("[0]", "", transfers))]
+transfersValDecreasing <- order(transfersNz, decreasing = TRUE)
+
+transfersGz <- which(transfers > 0, useNames = TRUE)
+transfersKey <- fileNames[transfersGz]
+transfersKeyDecreasing <- order(transfersKey, decreasing = TRUE)
+
+barplot(as.matrix(transfersNz), main = "Occurrences of Transfers in Set Protocol Contracts", ylab="Total", beside = TRUE, col=rainbow(12), legend = (transfersKey))
+
+
 #find all functions with "transfer" in them
+
