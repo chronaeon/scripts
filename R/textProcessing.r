0596958a095597a78ef107595f80e18d6b329cf4
Micah Dameron <micahdameron@gmail.com>
2019-03-11 10:28:19 -0400

    modified:   R/textProcessing.r

--- R/textProcessing.r
+++ R/textProcessing.r
@@ -2,48 +2,44 @@
 
 listOfPackages <- c("ggplot2", "Rcpp", "stringr", "tokenizers", "tidyverse", "plyr", "tidytext", "dplyr", "reshape2", "tm", "ngram")
 new.packages <- listOfPackages[!(listOfPackages %in% installed.packages()[,"Package"])]
+
 if(length(new.packages)) install.packages(new.packages)
 
 lapply(listOfPackages, require, character.only = TRUE)
-
-#Scoping Solidity Code for an Audit
-
-
-
+rm(new.packages)
 
 
 solList <- list.files(path = "~/set-protocol-contracts/contracts", pattern = "\\.sol$", recursive = TRUE, full.names = TRUE)
-#solList contains file locations
 
-
+solListFiltered <- gsub("^.*DappHub*$|^.*0x.*$|^.*test.*$|^.*kyber.*$|^.*mocks.*$|^.*node_modules.*$|^.*Migrations.*$|^.*coverageEnv.*$|^.*openzeppelin-solidity.*$|^.*cryptofin-solidity.*$|^.*0x.*$|^.*IERC20.*$|^.Bytes32.sol.*$|^.*Authorizable.sol.*$|^.*AddressArrayUtils.sol.*$|^.*CommonMath.sol.*$", "", solList, ignore.case = TRUE)
+solListClean <- solListFiltered[solListFiltered !=""]
+flatSol <- lapply(solListClean, readLines)
+cleanCode <- gsub("(?s)\\/\\*.*?\\*\\/|\\/\\/.*", "", flatSol, ignore.case = TRUE, perl = TRUE)
+cleanerCode <- cleanCode[cleanCode !=""]
 #flatten the Solidity files representing each as a single character vector
-flatCode <- lapply(solList, readLines)
+#solListFiltered <- lapply(solListClean, readLines)
 
 
 
 #extract all numbers in the code
-flatCodeNumbers <- str_extract_all(flatCode,"[0-9]+")
+#flatCodeNumbers <- str_extract_all(flatCode,"[0-9]+")
 
-options(max.print=10000000)
+#options(max.print=10000000)
 #flatCodeTable<-lapply(flatCodeNumbers, table)
 
 #z <- do.call(cbind, flatCodeTable[3])
 
-vector <- unlist(flatCodeNumbers)
+#vector <- unlist(flatCodeNumbers)
 
-numbers <- as.numeric(vector)
+#numbers <- as.numeric(vector)
 
-sortedNumbers <-  sort.int(numbers)
+#sortedNumbers <-  sort.int(numbers)
 
-countedNumbers <- count(numbers)
+#countedNumbers <- count(numbers)
 
-#removeSolBlockComments
-commentyCode <- grep("(?s)\\/\\*.*?\\*\\/", (flatCode), perl=TRUE, value=TRUE)
-commentlessCode <- stringr::str_replace_all(commentyCode, "(?s)\\/\\*.*?\\*\\/", " ")
-commentlessChars <- as.character(commentlessCode)
-ngCommentless <- tokenize_ngrams(commentlessChars)
-nMelt<-melt(ngCommentless)
-#removeSolBlockComments
+cleanSolChar <- as.character(cleanSol)
+cleanNg <- tokenize_ngrams(cleanSolChar)
+cleanMelt<-melt(cleanNg)
 
 sentencesList <- tokenize_sentences(solList)
 flatCharacter <- as.character(flatCode)
@@ -52,16 +48,15 @@ length(sentencesList)
 sapply(sentencesList, length)
 flatNgrams<-tokenize_ngrams(flatCharacter)
 
-
 flatNgrams$id <- rownames(flatNgrams)
 meltedNgrams<-melt(flatNgrams)
 meltedDf<-as.data.frame(meltedNgrams)
 rm(meltedNgrams)
-str_count(flatCode, "withdraw")
+str_count(flatSol, "withdraw")
 #find all functions with "withdraw" in them
 
-str_count(flatCode, "deposit")
+str_count(flatSol, "deposit")
 #find all functions with "deposit" in them
 
-str_count(flatCode, "transfer")
+str_count(flatSol, "transfer")
 #find all functions with "transfer" in them
