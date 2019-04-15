63a6073cc96a92473a970bb577c5ff657a91a234
Micah Dameron <micahdameron@gmail.com>
2019-04-14 20:39:59 -0600

    modified:   compSync.sh

--- compSync.sh
+++ compSync.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 rsync -rP --copy-links --ignore-existing micah@192.168.1.41:/home/micah/* ./ 
-rsync -rP --copy-links --ignore-existing micah@192.168.1.41:/home/micah/.[^.]*
+rsync -rP --copy-links --ignore-existing micah@192.168.1.41:/home/micah/.[^.]*micah@hpfreely:~$ rsync -rP --copy-links --ignore-existing micah@192.168.1.41:/home/micah/.* ./
+
